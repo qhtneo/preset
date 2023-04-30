18878a0e5dfded0bb6be369726c6584d9534740a
qhtneo <hj9671@gmail.com>
2023-04-30 19:09:03 +0900

    initial commit

---
+++ src/main/java/com/project/preset/PresetApplication.java
@@ -0,0 +1,13 @@
+package com.project.preset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/preset/PresetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.preset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
